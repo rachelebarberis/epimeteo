493b2069bb28792495d19741d367fca8397adb2a
Rachele Barberis <rachele.barberis.12@gmail.com>
2025-01-17 12:58:07 +0100

    commit giornaliero finito

--- src/Components/CityInfoDay.jsx
+++ src/Components/CityInfoDay.jsx
@@ -8,26 +8,38 @@ const CityWeatherDisplay = ({ cityInfo }) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
-        <Col xs={12} sm={8} md={6}>
+        <Col xs={12} sm={8} md={10}>
           <Card className="shadow-lg" style={{ borderRadius: "10px" }}>
-            <Card.Body>
-              <Card.Title className="text-center">
-                <h3>{cityInfo.name}</h3>
+            <Row>
+              <Col md={6}>
+                {" "}
+                <Card.Title className="text-center">
+                  <h3>{cityInfo.name}</h3>
+                </Card.Title>
+              </Col>
+              <Col md={6}></Col>
+            </Row>
+            <Row>
+              <Col md={6}>
+                {" "}
                 <img
                   src={`http://openweathermap.org/img/wn/${cityInfo.weather[0].icon}.png`}
                   alt={cityInfo.weather[0].description}
                   style={{ width: "50px", height: "50px" }}
                 />
-              </Card.Title>
-              <Card.Text className="text-center">
-                <p>{cityInfo.weather[0].description}</p>
-                <h4>Temperatura: {cityInfo.main.temp}°C</h4>
-                <h4>Temperatura minima: {cityInfo.main.temp_min}°C</h4>
-                <h4>Temperatura massima: {cityInfo.main.temp_max}°C</h4>
-                <p>Umidità: {cityInfo.main.humidity}%</p>
-                <p>Vento: {cityInfo.wind.speed} km/h</p>
-              </Card.Text>
-            </Card.Body>
+                <p>{cityInfo.weather[0].description}</p>{" "}
+              </Col>
+
+              <Col md={6}>
+                <Card.Text className="text-start">
+                  <h4>Temperatura: {cityInfo.main.temp}°C</h4>
+                  <h4>Temperatura minima: {cityInfo.main.temp_min}°C</h4>
+                  <h4>Temperatura massima: {cityInfo.main.temp_max}°C</h4>
+                  <p>Umidità: {cityInfo.main.humidity}%</p>
+                  <p>Vento: {cityInfo.wind.speed} km/h</p>
+                </Card.Text>
+              </Col>
+            </Row>
           </Card>
         </Col>
       </Row>
